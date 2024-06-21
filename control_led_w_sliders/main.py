6ccc6e0bfd153c32522dd42c5ab7be37bf14c52c
dobanov <117526546+dobanov@users.noreply.github.com>
2024-06-21 11:22:39 +0300

    Update main.py

--- control_led_w_sliders/main.py
+++ control_led_w_sliders/main.py
@@ -1,13 +1,16 @@
-from machine import ADC, Pin, PWM
+from machine import Pin, PWM, reset, Timer
 import socket
 
-# Initialize the hall sensor and PWM for LEDs
+# Initialize PWM for LEDs
 led_14 = PWM(Pin(14), freq=1000)
 led_12 = PWM(Pin(12), freq=1000)
 led_13 = PWM(Pin(13), freq=1000)
 
-def web_page():
+# Watchdog Timer setup (2 minutes timeout)
+wdt = Timer(1)
+wdt.init(period=120000, mode=Timer.PERIODIC, callback=lambda t: reset())
 
+def web_page():
     # Create the HTML page with sliders
     html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
@@ -28,35 +31,47 @@ def web_page():
     </body></html>"""
     return html
 
+def handle_client(conn):
+    try:
+        request = conn.recv(1024)
+        request = str(request)
+        print('Content = %s' % request)
+
+        # Parse the request to control LED brightness
+        if '/?led14=' in request:
+            brightness = int(request.split('=')[1].split(' ')[0])
+            print('LED 14 Brightness:', brightness)
+            led_14.duty(brightness)
+        if '/?led12=' in request:
+            brightness = int(request.split('=')[1].split(' ')[0])
+            print('LED 12 Brightness:', brightness)
+            led_12.duty(brightness)
+        if '/?led13=' in request:
+            brightness = int(request.split('=')[1].split(' ')[0])
+            print('LED 13 Brightness:', brightness)
+            led_13.duty(brightness)
+
+        response = web_page()
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.sendall(response)
+    except Exception as e:
+        print('Error:', e)
+        reset()
+    finally:
+        conn.close()
+
 # Set up the socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
 while True:
-    conn, addr = s.accept()
-    print('Got a connection from %s' % str(addr))
-    request = conn.recv(1024)
-    request = str(request)
-    print('Content = %s' % request)
-
-    # Parse the request to control LED brightness
-    if '/?led14=' in request:
-        brightness = int(request.split('=')[1].split(' ')[0])
-        print('LED 14 Brightness:', brightness)
-        led_14.duty(brightness)
-    if '/?led12=' in request:
-        brightness = int(request.split('=')[1].split(' ')[0])
-        print('LED 12 Brightness:', brightness)
-        led_12.duty(brightness)
-    if '/?led13=' in request:
-        brightness = int(request.split('=')[1].split(' ')[0])
-        print('LED 13 Brightness:', brightness)
-        led_13.duty(brightness)
-
-    response = web_page()
-    conn.send('HTTP/1.1 200 OK\n')
-    conn.send('Content-Type: text/html\n')
-    conn.send('Connection: close\n\n')
-    conn.sendall(response)
-    conn.close()
+    try:
+        conn, addr = s.accept()
+        print('Got a connection from %s' % str(addr))
+        handle_client(conn)
+    except Exception as e:
+        print('Error:', e)
+        reset()
