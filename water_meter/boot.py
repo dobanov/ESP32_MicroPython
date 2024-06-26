749d37baffc1c094d3dbe4dd5497c6fed1cd2904
dobanov <117526546+dobanov@users.noreply.github.com>
2024-06-26 12:25:51 +0300

    Update boot.py

--- water_meter/boot.py
+++ water_meter/boot.py
@@ -10,33 +10,40 @@ gc.collect()
 ssid = 'your_SSID'
 password = 'your_PASSWORD'
 
-station = network.WLAN(network.STA_IF)
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(ssid, password)
 
-station.active(True)
-station.connect(ssid, password)
+# Increase timeout for Wi-Fi connection
+timeout = 20  # seconds
+start_time = utime.time()
+while not wifi.isconnected():
+    if utime.time() - start_time > timeout:
+        print("Wi-Fi connection timed out")
+        break
+    utime.sleep(1)
 
-while station.isconnected() == False:
-  pass
+if wifi.isconnected():
+    print('Connected to Wi-Fi')
+    print('IP address:', wifi.ifconfig()[0])
+    
+    # List of NTP servers to try
+    ntp_servers = ['pool.ntp.org', 'ntp1.ntp-servers.net', 'time.microsoft.com']
+    
+    # Attempt to synchronize time with multiple NTP servers
+    for server in ntp_servers:
+        try:
+            ntptime.host = server
+            ntptime.settime()
+            print(f'Time synchronized with {server}')
+            break  # Exit loop if synchronization successful
+        except OSError as e:
+            print(f"Failed to synchronize time with {server}: {e}")
 
-print('Connected to Wi-Fi')
-print(station.ifconfig())
+    # Print current local time after synchronization
+    current_time = utime.localtime()
+    formatted_time = f"{current_time[0]}/{current_time[1]:02}/{current_time[2]:02} {current_time[3]:02}:{current_time[4]:02}:{current_time[5]:02}.{utime.ticks_ms() % 1000}"
+    print('Current local time:', formatted_time)
 
-# Synchronize time using NTP
-ntptime.host = 'pool.ntp.org'
-ntptime.settime()
-
-# Set timezone offset (UTC+3)
-timezone_offset = 3 * 3600  # 3 hours in seconds
-
-# Get current time and apply timezone offset
-current_time = utime.time() + timezone_offset
-local_time = utime.localtime(current_time)
-
-# Format the time
-year, month, day, hour, minute, second, weekday, yearday = local_time
-milliseconds = utime.ticks_ms() % 1000
-
-formatted_time = f"{year}/{month:02d}/{day:02d} {hour:02d}:{minute:02d}:{second:02d}.{milliseconds:03d}"
-
-print('Time synchronized')
-print('Current local time:', formatted_time)
+else:
+    print("Failed to connect to Wi-Fi")
