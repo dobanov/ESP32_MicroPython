ae62c49ac23ba937208f8ed7eb347a36d9c82547
dobanov <117526546+dobanov@users.noreply.github.com>
2024-07-02 16:15:57 +0300

    Create config.py

---
+++ impulses_count/config.py
@@ -0,0 +1,7 @@
+# Wi-Fi credentials
+WIFI_SSID = 'your_SSID'
+WIFI_PASSWORD = 'your_PASSWORD'
+
+# Telegram bot token and chat IDs
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_IDS = "YOUR_CHAT_ID1,YOUR_CHAT_ID2"  # Separate multiple chat IDs with commas
