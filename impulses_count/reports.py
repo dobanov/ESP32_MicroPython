02a522aa94d0ffe1bc2b105a36bd9cd976fdcb7c
dobanov <117526546+dobanov@users.noreply.github.com>
2024-07-02 12:00:16 +0300

    Update reports.py

--- impulses_count/reports.py
+++ impulses_count/reports.py
@@ -1,22 +1,5 @@
 from telegram import send_text_to_telegram
-
-# Function to read counter from file
-def read_counter_from_file(filename):
-    try:
-        with open(filename, 'r') as file:
-            return int(file.read())
-    except (OSError, ValueError) as e:
-        print(f"Failed to read from {filename}. Creating file...")
-        write_counter_to_file(filename, 0)  # Create file if it doesn't exist
-        return 0
-
-# Function to write counter to file
-def write_counter_to_file(filename, counter):
-    try:
-        with open(filename, 'w') as file:
-            file.write(str(counter))
-    except OSError as e:
-        print(f"Failed to write to {filename}: {e}")
+from file_rw import read_counter_from_file, write_counter_to_file
 
 # Function to calculate consumption and send report
 def send_report(bot_token, chat_ids, period):
