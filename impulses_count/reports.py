794c59152a581e04528ec7d6685ffba0450fb29a
dobanov <117526546+dobanov@users.noreply.github.com>
2024-07-02 11:11:23 +0300

    Update reports.py

--- impulses_count/reports.py
+++ impulses_count/reports.py
@@ -34,6 +34,6 @@ def send_report(bot_token, chat_ids, period='daily'):
     write_counter_to_file(f'hot_last_{period}', hot_total)
     write_counter_to_file(f'cold_last_{period}', cold_total)
 
-    message = f"{period.capitalize()} report: {hot_this_period}0 liters of hot water and {cold_this_period}0 liters of cold water"
+    message = f"{period} report: {hot_this_period}0 liters of hot water and {cold_this_period}0 liters of cold water"
     send_text_to_telegram(bot_token, chat_ids, message)
     print(message)
