cffa053d5bc0827af81e6aebea7b2bbf11c0c72d
dobanov <117526546+dobanov@users.noreply.github.com>
2024-07-03 11:29:53 +0300

    Update reports.py

--- impulses_count/reports.py
+++ impulses_count/reports.py
@@ -1,13 +1,14 @@
 from telegram import send_text_to_telegram
 from file_rw import read_counter_from_file, write_counter_to_file
+from config import BOT_TOKEN, CHAT_ID
 
 # Function to calculate consumption and send report
-def send_report(bot_token, chat_ids, period):
+def send_report(period):
 
-#    print(f"Preparing to send {period} report.")  # Debugging line
-#    print(f"Type of period: {type(period)}")
-#    print(f"Period value: '{period}'")
-    
+    print(f"Preparing to send {period} report.")  # Debugging line
+    print(f"Type of period: {type(period)}")
+    print(f"Period value: '{period}'")
+
     hot_last_period = read_counter_from_file(f'hot_last_{period}')
     hot_total = read_counter_from_file('hot')
     cold_last_period = read_counter_from_file(f'cold_last_{period}')
@@ -20,5 +21,5 @@ def send_report(bot_token, chat_ids, period):
     write_counter_to_file(f'cold_last_{period}', cold_total)
 
     message = f"{period} report: {hot_this_period}0 liters of hot water and {cold_this_period}0 liters of cold water"
-    send_text_to_telegram(bot_token, chat_ids, message)
-    print(message)
+    send_text_to_telegram(BOT_TOKEN, CHAT_ID, message)
+    # print(message)
